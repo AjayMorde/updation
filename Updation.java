ca026ea9030f6d4fb8e46f2ab2507ec4bbd7d20a
AjayMorde <103033106+AjayMorde@users.noreply.github.com>
2022-04-08 01:11:03 +0530

    Add files via upload

---
+++ Updation.java
@@ -0,0 +1,18 @@
+package welcomeToJava;
+
+public class Updation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num ;
+		num =20;
+		System.out.println(num+2);
+				num = num+6;
+						System.out.println(num);
+		num= num+6;
+		System.out.println(num);
+		
+				
+	}
+
+}
